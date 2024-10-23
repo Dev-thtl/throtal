4e72afa84fee7ec0938880fd299b7033b4b881ff
Dev-thtl <neeraj.wibmo1062@gmail.com>
2024-10-24 00:05:52 +0530

    User Profile changrs

---
+++ src/main/java/com/throtl/user/rsaModel/Policy.java
@@ -0,0 +1,95 @@
+package com.throtl.user.rsaModel;
+
+public class Policy {
+
+    private double membership_amount;
+    private String vehicle_type_name;
+    private String engine_no;
+    private String vehicle_no;
+    private String chassis_no;
+    private String membership_no;
+    private String vehicle_brand_name;
+    private String id;
+    private String policy_duration;
+    private String vehicle_model_name;
+
+    public double getMembership_amount() {
+        return membership_amount;
+    }
+
+    public void setMembership_amount(double membership_amount) {
+        this.membership_amount = membership_amount;
+    }
+
+    public String getVehicle_type_name() {
+        return vehicle_type_name;
+    }
+
+    public void setVehicle_type_name(String vehicle_type_name) {
+        this.vehicle_type_name = vehicle_type_name;
+    }
+
+    public String getEngine_no() {
+        return engine_no;
+    }
+
+    public void setEngine_no(String engine_no) {
+        this.engine_no = engine_no;
+    }
+
+    public String getVehicle_no() {
+        return vehicle_no;
+    }
+
+    public void setVehicle_no(String vehicle_no) {
+        this.vehicle_no = vehicle_no;
+    }
+
+    public String getChassis_no() {
+        return chassis_no;
+    }
+
+    public void setChassis_no(String chassis_no) {
+        this.chassis_no = chassis_no;
+    }
+
+    public String getMembership_no() {
+        return membership_no;
+    }
+
+    public void setMembership_no(String membership_no) {
+        this.membership_no = membership_no;
+    }
+
+    public String getVehicle_brand_name() {
+        return vehicle_brand_name;
+    }
+
+    public void setVehicle_brand_name(String vehicle_brand_name) {
+        this.vehicle_brand_name = vehicle_brand_name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPolicy_duration() {
+        return policy_duration;
+    }
+
+    public void setPolicy_duration(String policy_duration) {
+        this.policy_duration = policy_duration;
+    }
+
+    public String getVehicle_model_name() {
+        return vehicle_model_name;
+    }
+
+    public void setVehicle_model_name(String vehicle_model_name) {
+        this.vehicle_model_name = vehicle_model_name;
+    }
+}
